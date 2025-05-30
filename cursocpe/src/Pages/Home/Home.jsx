8242a8a8ce0de03b9c9008411f2a68b0ff4ee133
LuisOtavi0 <luis-o-ss@hotmail.com>
2025-05-29 23:55:31 -0300

    pequena atualizacao

--- cursocpe/src/Pages/Home/Home.jsx
+++ cursocpe/src/Pages/Home/Home.jsx
@@ -1,24 +1,52 @@
-import { Container, Formulario, Input, Button} from "./Styles";
+import { Container, SpinnerAmarelo} from "./Styles";
 import {useForm} from "react-hook-form";
 import { useCreateUser } from "../../hooks/user";
+import { useEffect, useState } from "react";
+import api from "../../services/api/api";
+import {Usuario} from "../../Components";
+
     //Hooks
 export default function Home(){
+    const [usuarios, setUsuarios] = useState([]);
+    const [carregando, setCarregando] = useState(false);
+
+    console.log(usuarios);
+
+    const getUsuarios = async () => {
+        try {
+            setCarregando(true);
+            const res = await api.get("/usuarios");
+            setUsuarios(res.data);     
+
+        } catch (erro) {
+            console.error(erro);
+            alert(erro.response.data.message);
+        } finally {
+            setCarregando(false);
+        }
+    };
+
+    useEffect(() => {
+        getUsuarios();
+    }, []);
+
     const {handleSubmit, register, formState: {errors},} = useForm({});
     const {mutate: postUser, isPending} = useCreateUser({});
-    //onSubmit
+
+    if (carregando) return (
+            <Container style={{display: 'flex', justifyContent: 'center',
+            alignItems: 'center', height: '100vh'}}>
+                <SpinnerAmarelo size="large" />
+            </Container>
+        );
+
     function response(data) {
         postUser(data);
     };
 
     return(
         <Container>
-            <Formulario onSubmit={handleSubmit(response)}>
-                <Input {...register("name")} placeholder="nome" autoComplete="name"/>
-                <Input {...register("type")} placeholder="tipo"/>
-                <Input {...register("role")} placeholder="cargo"/>
-
-                <Button>enviar</Button>
-            </Formulario>
+            {usuarios.map((usuario) => (<Usuario usuario={usuario} />))}
         </Container>
     );
 }
--- cursocpe/src/Pages/Home/Styles.js
+++ cursocpe/src/Pages/Home/Styles.js
@@ -1,4 +1,12 @@
 import styled from "styled-components";
+import { Spin } from "antd";
+
+
+export const SpinnerAmarelo = styled(Spin)`
+  .ant-spin-dot-item {
+    background-color: yellow !important;
+  }
+`;
 
 export const Container = styled.div`
     background-color: #000;
@@ -7,55 +15,7 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
     padding-top: 70px;
-`;
 
-export const Formulario = styled.form`
-    width: 100%;
-    max-width: 400px;
-    display: flex;
     flex-direction: column;
-    align-items: center;
     gap: 15px;
-    padding: 0 15px;
-
-    @media (max-width: 320px) {
-    gap: 10px;
-    }
-`;
-
-export const Input = styled.input`
-    width: 100%;
-    padding: 12px 16px;
-    border-radius: 20px;
-    border: none;
-    outline: none;
-    font-size: 14px;
-    background-color: #f0f0f0;
-
-    @media (max-width: 320px) {
-    padding: 10px 14px;
-    font-size: 13px;
-    }
-`;
-
-export const Button = styled.button`
-    width: 50%;
-    padding: 12px;
-    background-color: #Fec937;
-    color: #000;
-    font-weight: bold;
-    border: none;
-    border-radius: 20px;
-    cursor: pointer;
-    transition: 0.3s ease;
-
-    &:hover {
-        background-color: #e6c300;
-    }
-
-    @media (max-width: 320px) {
-    width: 50%;
-    padding: 10px;
-    font-size: 14px;
-    }
 `;
