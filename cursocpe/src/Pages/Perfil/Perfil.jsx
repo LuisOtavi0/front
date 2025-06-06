c8c5a34265a15c0e600c4403c9e049aa1e57f6ad
LuisOtavi0 <luis-o-ss@hotmail.com>
2025-06-05 23:57:44 -0300

    PAgina de perfil

--- cursocpe/src/Pages/Perfil/Perfil.jsx
+++ cursocpe/src/Pages/Perfil/Perfil.jsx
@@ -1,13 +1,32 @@
-import {Container } from "./Styles";
+import React, { useState } from "react";
+import {Container, ModalOverlay, ModalContent, InputField, BotaoEditar, BotaoSalvar, BotaoCancelar, TituloModal } from "./Styles";
 import useAuthStore from "../../stores/auth";
 import { Usuario } from "../../Components";
 
 export default function Perfil() {
     const usuario = useAuthStore((state) => state.usuario);
+    const [modalAberto, setModalAberto] = useState(false);
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [cargo, setCargo] = useState("")
 
     return (
         <Container>
             <Usuario usuario={usuario} />
+            <BotaoEditar onClick={() => setModalAberto(true)}>Editar</BotaoEditar>
+
+            {modalAberto && (
+                <ModalOverlay>
+                    <ModalContent>
+                        <TituloModal>Editar usuário</TituloModal>
+                        <InputField placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+                        <InputField placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <InputField placeholder="Cargo" value={cargo} onChange={(e) => setCargo(e.target.value)} />
+                        <BotaoSalvar>SALVAR</BotaoSalvar>
+                        <BotaoCancelar onClick={() => setModalAberto(false)}>✕</BotaoCancelar>
+                    </ModalContent>
+                </ModalOverlay>
+            )}
         </Container>
     );
 }
